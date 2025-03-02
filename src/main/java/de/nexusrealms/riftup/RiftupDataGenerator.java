1aec0455972a07b24071bcd8ebb3f78f7fdb718b
farpo <77583105+farpo@users.noreply.github.com>
2025-03-02 15:51:20 +0100

    Initial commit

---
+++ src/main/java/de/nexusrealms/riftup/RiftupDataGenerator.java
@@ -0,0 +1,11 @@
+package de.nexusrealms.riftup;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RiftupDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
